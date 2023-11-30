67e13bb71c6afa2aa1430db3984d7054360288dd
tmatthews092 <tmatthews092@gmail.com>
2023-11-30 13:05:28 -0700

    - added new function for file iteration to identify name, seq - updated json return - algo now O(n*m)

--- python/main.py
+++ python/main.py
@@ -1,6 +1,7 @@
 import json
 import boto3
 import logging
+import re
 from datetime import datetime, timezone
 
 logger = logging.getLogger()
@@ -9,20 +10,34 @@ s3 = boto3.client('s3')
 substring_to_search = 'AACGCT'
 
 def lambda_handler(event, context):
+    # match data init variables
+    sequence_data = []
+
     # retrieve bucket name and file_key from the S3 event
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     file_key = event['Records'][0]['s3']['object']['key']
     # get the object
     obj = s3.get_object(Bucket=bucket_name, Key=file_key)
     #get lines of file
-    file_content = obj['Body'].read().decode('utf-8')
-    matches = file_content.count(substring_to_search)
+    file_content = obj['Body'].read().decode('utf-8').splitlines()
+    for name, seq in extract_name_seq(file_content):
+        matches = 0
+        match_start_positions = []
+        for match in re.finditer(substring_to_search, seq):
+            matches += 1
+            match_start_positions.append(match.start())
+        sequence_data.append({
+            'name': name,
+            'sequence': seq,
+            'matches': matches,
+            'match_start_positions': match_start_positions
+        })
 
     # Format all data to be inserted in s3 file
     match_data = {
         "file_name": file_key,
         "substring_to_search": substring_to_search,
-        "matches": matches,
+        "sequences_data": sequence_data,
         "timestamp": datetime.now(timezone.utc)
     }
     
@@ -39,8 +54,20 @@ def lambda_handler(event, context):
     # upload s3 file back to bucket in results folder
     s3.put_object(Bucket=bucket_name, Key=s3_path, Body=encoded_string)
 
-    logger.info(matches)
+    logger.info(match_data)
     return {
         'statusCode': 200,
-        'body': matches
+        'body': match_data
     }
+
+# https://stackoverflow.com/questions/29805642/learning-to-parse-a-fasta-file-with-python
+def extract_name_seq(fc):
+    name, seq = None, []
+    for line in fc:
+        line = line.rstrip()
+        if line.startswith(">"):
+            if name: yield (name, ''.join(seq))
+            name, seq = line, []
+        else:
+            seq.append(line)
+    if name: yield (name, ''.join(seq))
